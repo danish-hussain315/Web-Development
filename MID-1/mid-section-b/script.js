b46a0e1be98fbc62f1fb422b953cec1d9e919845
danish-hussain315 <danishhussain.cs.official@gmail.com>
2024-04-02 14:54:26 +0500

    last Commit

--- MID-1/mid-section-b/script.js
+++ MID-1/mid-section-b/script.js
@@ -1,4 +1,20 @@
-const container = document.getElementsByClassName('container');
 
-const img = document.createElement('div');
-img.classList.add('img-container');
+const container = document.getElementByClassName('container');
+
+const imgContainer = document.getElementByClassName("imgContainer");
+
+const viewImage = document.createElement('img');
+viewImage.src = './one.svg';
+
+imgContainer.appendChild(viewImage);
+
+const prebtn = document.getElementByClassName('left');
+prebtn.onClick = showPreviousImage;
+
+const nextbtn = document.getElementByClassName('right');
+prebtn.onClick = showPreviousImage;
+
+
+function showPreviousImage(){
+
+}
